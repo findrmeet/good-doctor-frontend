205902c8a4af9aeed38ec5ba2ca99be17c36e7c5
findrmeet <findrmeet@gmail.com>
2026-01-08 13:13:59 +0600

    animated scrollbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero/Hero";
+import Marquee from "@/components/Marquee/Marquee";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Marquee />
     </>
   );
 }
---
+++ src/components/Marquee/Marquee.tsx
@@ -0,0 +1,11 @@
+const Marquee = () => {
+  return (
+    <div className="bg-blue-500 overflow-hidden py-2">
+      <ul className="loop-scroll flex gap-8 w-max text-white">
+        <li>Medicines & Supplies</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Marquee;
