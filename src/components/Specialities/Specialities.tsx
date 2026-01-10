74afffe0191a6d3f17b4c22dc42afda7fd7d3456
findrmeet <findrmeet@gmail.com>
2026-01-10 12:35:05 +0600

    spaciality up

--- src/components/Specialities/Specialities.tsx
+++ src/components/Specialities/Specialities.tsx
@@ -6,22 +6,25 @@ import specialistData from "../../../public/specilist.data.json";
 
 const Specialities = () => {
   return (
-    <section className="space-y-10 bg-linear-to-br from-green-100 via-white to-blue-100">
+    <section className="space-y-10 bg-gradient-to-br from-green-100 via-white to-blue-100">
       <div className="custom-container py-18 space-y-10">
         <SectionInfo
           title="Your Health, Our Priority — 33+ Experts Across Specialties"
           subtitle="Talk to doctors who listen, guide, and care."
         />
 
-        <div className="grid grid-cols-7 bg-white gap-4">
-          {specialistData.map((specialist) => (
-            <SpecialitiesCard
-              key={specialist.id}
-              src={specialist.src}
-              name={specialist.name}
-              details={specialist.details}
-            />
-          ))}
+        {/* Use border-separate and negative margins */}
+        <div className="bg-white">
+          <div className="grid grid-cols-7">
+            {specialistData.map((specialist) => (
+              <SpecialitiesCard
+                key={specialist.id}
+                src={specialist.src}
+                name={specialist.name}
+                details={specialist.details}
+              />
+            ))}
+          </div>
         </div>
 
         <div className="flex justify-center">
--- src/components/Specialities/SpecialitiesCard.tsx
+++ src/components/Specialities/SpecialitiesCard.tsx
@@ -12,7 +12,7 @@ const SpecialitiesCard: React.FC<SpecialitiesCardProps> = ({
   details,
 }) => {
   return (
-    <div className=" flex justify-center flex-col gap-2 py-4 px-2 items-center hover:bg-blue-500 hover:text-white cursor-pointer transition">
+    <div className="border border-gray-100 flex justify-center flex-col gap-2 py-4 px-2 items-center hover:bg-blue-500 hover:text-white cursor-pointer transition">
       <Image src={src} height={72} width={72} alt={name} />
       <h2 className="font-semibold text-center">{name}</h2>
       <p className="text-xs font-medium text-center">{details}</p>
