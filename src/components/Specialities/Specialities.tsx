d23a335f3af664fcae5ef4e077fdcd0a3c2730f7
findrmeet <findrmeet@gmail.com>
2026-01-10 12:16:12 +0600

    speciality card

--- src/components/Specialities/Specialities.tsx
+++ src/components/Specialities/Specialities.tsx
@@ -2,59 +2,30 @@ import { ChevronDown } from "lucide-react";
 import PrimaryBtn from "../Button/PrimaryBtn";
 import SectionInfo from "../SectionInfo/SectionInfo";
 import SpecialitiesCard from "./SpecialitiesCard";
+import specialistData from "../../../public/specilist.data.json";
 
 const Specialities = () => {
   return (
-    <section className="space-y-10   bg-linear-to-br from-green-100 via-white to-blue-100">
-      <div className="custom-container py-18">
+    <section className="space-y-10 bg-linear-to-br from-green-100 via-white to-blue-100">
+      <div className="custom-container py-18 space-y-10">
         <SectionInfo
           title="Your Health, Our Priority — 33+ Experts Across Specialties"
           subtitle="Talk to doctors who listen, guide, and care."
         />
-        <div className="grid grid-cols-7 ">
-          <SpecialitiesCard
-            src={"/General Physician.png"}
-            name={"General Physician"}
-            details="Common illnesses, fever & checkups"
-          />
-          <SpecialitiesCard
-            src={"/General Physician.png"}
-            name={"General Physician"}
-            details="Common illnesses, fever & checkups"
-          />
-          <SpecialitiesCard
-            src={"/General Physician.png"}
-            name={"General Physician"}
-            details="Common illnesses, fever & checkups"
-          />
-          <SpecialitiesCard
-            src={"/General Physician.png"}
-            name={"General Physician"}
-            details="Common illnesses, fever & checkups"
-          />
-          <SpecialitiesCard
-            src={"/General Physician.png"}
-            name={"General Physician"}
-            details="Common illnesses, fever & checkups"
-          />
-          <SpecialitiesCard
-            src={"/General Physician.png"}
-            name={"General Physician"}
-            details="Common illnesses, fever & checkups"
-          />
-          <SpecialitiesCard
-            src={"/General Physician.png"}
-            name={"General Physician"}
-            details="Common illnesses, fever & checkups"
-          />
-          <SpecialitiesCard
-            src={"/General Physician.png"}
-            name={"General Physician"}
-            details="Common illnesses, fever & checkups"
-          />
+
+        <div className="grid grid-cols-7 bg-white gap-4">
+          {specialistData.map((specialist) => (
+            <SpecialitiesCard
+              key={specialist.id}
+              src={specialist.src}
+              name={specialist.name}
+              details={specialist.details}
+            />
+          ))}
         </div>
+
         <div className="flex justify-center">
-          <PrimaryBtn className="flex gap-1 items-center py-3  px-4">
+          <PrimaryBtn className="flex gap-1 items-center py-3 px-4">
             View More Specialties <ChevronDown size={20} />
           </PrimaryBtn>
         </div>
