a6f37a7d59d932fa21c2cb43d260ed1b78bf3309
findrmeet <findrmeet@gmail.com>
2026-01-10 11:15:36 +0600

    update the marquee animation

--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -3,60 +3,62 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 
+const items = [
+  "Medicines & Supplies",
+  "Talk to Doctor",
+  "Lab Testing Services",
+  "Home Care Services",
+  "Health Care Services",
+  "Hospital & Clinics",
+  "Dental Care Services",
+  "Physiotherapy",
+  "Vaccination Programs",
+  "Nutrition & Diet Consultation",
+  "Mental Health Support",
+  "Ambulance Services",
+  "Eye Care & Optometry",
+  "Hearing & Audiology Services",
+  "Chiropractic Services",
+  "Wellness Programs",
+  "Home Laboratory Testing",
+  "Telemedicine Consultations",
+  "Cardiology Services",
+  "Pediatrics Care",
+  "Geriatric Services",
+];
+
 const Marquee = () => {
+  const renderItems = () =>
+    items.flatMap((item, idx) => [
+      <li key={`item-${idx}`} className="flex items-center">
+        {item}
+      </li>,
+      idx !== items.length - 1 && (
+        <Image
+          key={`line-${idx}`}
+          src="/line.svg"
+          width={64}
+          height={1}
+          alt="line"
+          className="mx-4"
+        />
+      ),
+    ]);
+
   return (
     <div className="bg-blue-500 overflow-hidden py-3 z-10">
       <motion.ul
-        className="flex gap-4 whitespace-nowrap text-white"
-        animate={{ x: ["100%", "-100%"] }}
+        className="flex whitespace-nowrap text-white"
+        animate={{ x: ["0%", "-50%"] }}
         transition={{
-          duration: 100, // adjust speed
+          duration: 50, // adjust speed
           ease: "linear",
-          repeat: Infinity, // infinite loop
+          repeat: Infinity,
         }}
       >
-        <li>Medicines & Supplies</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Talk to Doctor</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Lab Testing Services</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Home Care Services</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Health Care Services</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Hospital & Clinics</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Dental Care Services</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Physiotherapy</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Vaccination Programs</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Nutrition & Diet Consultation</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Mental Health Support</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Ambulance Services</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Eye Care & Optometry</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Hearing & Audiology Services</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Chiropractic Services</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Wellness Programs</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Home Laboratory Testing</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Telemedicine Consultations</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Cardiology Services</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Pediatrics Care</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
-        <li>Geriatric Services</li>
-        <Image src="/line.svg" width={64} height={1} alt="line" />
+        {/* Duplicate content for seamless scroll */}
+        <>{renderItems()}</>
+        <>{renderItems()}</>
       </motion.ul>
     </div>
   );
