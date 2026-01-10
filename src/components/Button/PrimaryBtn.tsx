3932e43ea2b9358bd9fc33a5e887b4d47bd8508c
findrmeet <findrmeet@gmail.com>
2026-01-10 12:44:22 +0600

    update

--- src/components/Button/PrimaryBtn.tsx
+++ src/components/Button/PrimaryBtn.tsx
@@ -1,14 +1,17 @@
 type PrimaryBtnProps = {
   className?: string;
   children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 const PrimaryBtn: React.FC<PrimaryBtnProps> = ({
+  onClick,
   className = "",
   children,
 }) => {
   return (
     <button
+      onClick={onClick}
       className={`cursor-pointer bg-blue-500 rounded-full text-white hover:bg-blue-600 ${className} transition-colors duration-300 text-nowrap`}
     >
       {children}
--- src/components/Specialities/Specialities.tsx
+++ src/components/Specialities/Specialities.tsx
@@ -1,22 +1,33 @@
+"use client";
 import { ChevronDown } from "lucide-react";
 import PrimaryBtn from "../Button/PrimaryBtn";
 import SectionInfo from "../SectionInfo/SectionInfo";
 import SpecialitiesCard from "./SpecialitiesCard";
 import specialistData from "../../../public/specilist.data.json";
+import { useState } from "react";
 
 const Specialities = () => {
+  const [showAllSpecialities, setShowAllSpecialities] =
+    useState<boolean>(false);
+
+  const displayedSpecialists = showAllSpecialities
+    ? specialistData
+    : specialistData.slice(0, 14); // only first 14
+
+  const toggleCardVisibility = () => {
+    setShowAllSpecialities(!showAllSpecialities);
+  };
   return (
-    <section className="space-y-10 bg-gradient-to-br from-green-100 via-white to-blue-100">
+    <section className="space-y-10 bg-linear-to-br from-green-100 via-white to-blue-100">
       <div className="custom-container py-18 space-y-10">
         <SectionInfo
           title="Your Health, Our Priority — 33+ Experts Across Specialties"
           subtitle="Talk to doctors who listen, guide, and care."
         />
 
-        {/* Use border-separate and negative margins */}
         <div className="bg-white">
-          <div className="grid grid-cols-7">
-            {specialistData.map((specialist) => (
+          <div className="grid grid-cols-7 ">
+            {displayedSpecialists.map((specialist) => (
               <SpecialitiesCard
                 key={specialist.id}
                 src={specialist.src}
@@ -28,8 +39,14 @@ const Specialities = () => {
         </div>
 
         <div className="flex justify-center">
-          <PrimaryBtn className="flex gap-1 items-center py-3 px-4">
-            View More Specialties <ChevronDown size={20} />
+          <PrimaryBtn
+            className="flex gap-1 items-center py-3 px-4"
+            onClick={toggleCardVisibility}
+          >
+            {showAllSpecialities
+              ? "View Less Specialties"
+              : "View More Specialties"}{" "}
+            <ChevronDown size={20} />
           </PrimaryBtn>
         </div>
       </div>
