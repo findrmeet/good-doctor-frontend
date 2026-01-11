0e0d4eeba8a7aa9dfb45f351fcee7b86502b148e
findrmeet <findrmeet@gmail.com>
2026-01-11 10:10:36 +0600

    choose by symptom responsivenss fixed

--- src/components/ChooseBySymptom/ChooseBySymptom.tsx
+++ src/components/ChooseBySymptom/ChooseBySymptom.tsx
@@ -37,7 +37,7 @@ const ChooseBySymptom = () => {
       />
 
       <motion.div
-        className="grid grid-cols-6 gap-6"
+        className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6"
         variants={containerVariants}
         initial="hidden"
         animate="visible"
